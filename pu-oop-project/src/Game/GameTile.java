4c21c0813b429a5e436e943c6593f512ab62d559
tihomir-dimitrov <63392523+tihomir-dimitrov@users.noreply.github.com>
2021-03-07 13:00:41 +0200

    Update GameTile.java

--- pu-oop-project/src/Game/GameTile.java
+++ pu-oop-project/src/Game/GameTile.java
@@ -7,75 +7,73 @@ public class GameTile {
 
     private int row;
     private int col;
-    private int tileSize;
 
     public GameTile(int row, int col) {
 
         this.row = row;
         this.col = col;
-        this.tileSize = 100;
     }
 
     public void render(Graphics g) {
-
-        int tileX = this.col * this.tileSize;
-        int tileY = this.row * this.tileSize;
-
-        if (
-                (this.row == 0 && this.col == 0) ||
-                (this.row == 0 && this.col == 2) ||
-                (this.row == 0 && this.col == 4) ||
-                (this.row == 0 && this.col == 6) ||
-                (this.row == 0 && this.col == 8) ||
-                (this.row == 1 && this.col == 1) ||
-                (this.row == 1 && this.col == 3) ||
-                (this.row == 1 && this.col == 5) ||
-                (this.row == 1 && this.col == 7) ||
-
-                (this.row == 5 && this.col == 0) ||
-                (this.row == 5 && this.col == 2) ||
-                (this.row == 5 && this.col == 4) ||
-                (this.row == 5 && this.col == 6) ||
-                (this.row == 6 && this.col == 1) ||
-                (this.row == 6 && this.col == 3) ||
-                (this.row == 6 && this.col == 5) ||
-                (this.row == 6 && this.col == 7)
-        ){
-            g.setColor(Color.GRAY);
-
-        } else if (
-                (this.row == 1 && this.col == 0) ||
-                (this.row == 1 && this.col == 2) ||
-                (this.row == 1 && this.col == 4) ||
-                (this.row == 1 && this.col == 6) ||
-                (this.row == 0 && this.col == 1) ||
-                (this.row == 0 && this.col == 3) ||
-                (this.row == 0 && this.col == 5) ||
-                (this.row == 0 && this.col == 7) ||
-
-                (this.row == 5 && this.col == 1) ||
-                (this.row == 5 && this.col == 3) ||
-                (this.row == 5 && this.col == 5) ||
-                (this.row == 5 && this.col == 7) ||
-                (this.row == 6 && this.col == 0) ||
-                (this.row == 6 && this.col == 2) ||
-                (this.row == 6 && this.col == 4) ||
-                (this.row == 6 && this.col == 6)
-        ){
-            g.setColor(Color.BLACK);
-
-        } else {
-            g.setColor(Color.WHITE);
-
-        }
-
-        g.fillRect(tileX, tileY, this.tileSize, this.tileSize);
+        BlockedTile(g);
+        boardGrid(g);
+        Grey(g);
+        Green(g);
     }
 
-    //Чертаене на решетката
+    //Чертаене на дъската
+    public void Green(Graphics g){
+        g.setColor(Color.GREEN);
+
+        g.fillRect(100,25,100,75);
+        g.fillRect(300,25,100,75);
+        g.fillRect(500,25,100,75);
+        g.fillRect(700,25,100,75);
+        g.fillRect(0,100,100,100);
+        g.fillRect(200,100,100,100);
+        g.fillRect(400,100,100,100);
+        g.fillRect(600,100,100,100);
+        g.fillRect(800,100,100,100);
+
+        g.fillRect(100,500,100,100);
+        g.fillRect(300,500,100,100);
+        g.fillRect(500,500,100,100);
+        g.fillRect(700,500,100,100);
+        g.fillRect(0,600,100,100);
+        g.fillRect(200,600,100,100);
+        g.fillRect(400,600,100,100);
+        g.fillRect(600,600,100,100);
+        g.fillRect(800,600,100,100);
+    }
+
+    public void Grey(Graphics g){
+        g.setColor(Color.GRAY);
+
+        g.fillRect(0,25,100,75);
+        g.fillRect(200,25,100,75);
+        g.fillRect(400,25,100,75);
+        g.fillRect(600,25,100,75);
+        g.fillRect(800,25,100,75);
+        g.fillRect(100,100,100,100);
+        g.fillRect(300,100,100,100);
+        g.fillRect(500,100,100,100);
+        g.fillRect(700,100,100,100);
+
+        g.fillRect(0,500,100,100);
+        g.fillRect(200,500,100,100);
+        g.fillRect(400,500,100,100);
+        g.fillRect(600,500,100,100);
+        g.fillRect(800,500,100,100);
+        g.fillRect(100,600,100,100);
+        g.fillRect(300,600,100,100);
+        g.fillRect(500,600,100,100);
+        g.fillRect(700,600,100,100);
+    }
+
+    //Чертаене на решетка
     void boardGrid(Graphics g) {
         g.setColor(Color.black);
-        for (int i = 0; i <= 660; i += 100) {
+        for (int i = 0; i <= 900; i += 100) {
             for (int j = 0; j <= 700; j += 100) {
                 g.drawRect(i, j, 100, 100);
             }
